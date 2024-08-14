e43b9bad997f099002f76a59267eeb59a5bacf4b
Roman Ožana <roman@ozana.cz>
2024-08-14 16:25:55 +0200

    Další svátky a kontrola icon

---
+++ test/check-icons.ts
@@ -0,0 +1,8 @@
+import test from 'ava';
+import data from '../lib/international-days.json';
+
+test('Kontrola přiřazení ikon', t => {
+	for (const day of data) {
+		t.true(day.icon !== undefined, `Icon not found for ${day.name}`);
+	}
+});
