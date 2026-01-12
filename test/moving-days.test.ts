f413e2a3b8e6eb91b1bb395b1d51f787b707f08a
Roman Ožana <roman@ozana.cz>
2026-01-12 09:03:11 +0100

    Fix tests

--- test/moving-days.test.ts
+++ test/moving-days.test.ts
@@ -14,6 +14,12 @@ describe("International Days Tests", () => {
 		expect(day?.name).toBe("Den uklízení pracovních stolů");
 	});
 
+	it("Den uklízení pracovních stolů (2026)", () => {
+		const days = getInternationalDays(new Date("2026-01-12"));
+		const day = days.find((day) => day.name === "Den uklízení pracovních stolů");
+		expect(day?.name).toBe("Den uklízení pracovních stolů");
+	});
+
 	it("Den matek (2024)", () => {
 		const days = getInternationalDays(new Date("2024-05-12"));
 		const day = days.find((day) => day.name === "Den matek");
